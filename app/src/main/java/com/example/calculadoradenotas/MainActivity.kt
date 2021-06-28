84710980b55bf75ab59b012374dc4e2347540113
Nicolas Brunaldi de Oliveira <nicolas.brunaldi@gmail.com>
2021-06-28 20:45:40 -0300

    Implementando a lógica do app

--- app/src/main/java/com/example/calculadoradenotas/MainActivity.kt
+++ app/src/main/java/com/example/calculadoradenotas/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.calculadoradenotas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val botaoCalcular = calcular
+        val resultado = resultados
+
+        botaoCalcular.setOnClickListener(){
+            val nota1 = Integer.parseInt(nota1.text.toString())
+            val nota2 = Integer.parseInt(nota2.text.toString())
+            val faltas = Integer.parseInt(faltas.text.toString())
+            var notaFinal = (nota1 + nota2)/2
+            if (notaFinal >= 7 && faltas in 0..10){
+                resultado.setText("Aluno Aprovado!\nNota Final: $notaFinal Faltas: $faltas")
+            }
+            else{
+                resultado.setText("Aluno Reprovado!")
+            }
+        }
     }
 }
